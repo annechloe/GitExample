a181ed688f9c48e8d635d7fd016e9b084054b7df
tst05 <tst05@LYTC-BUS05.ly.fastc.ie>
2018-08-29 15:20:16 +0100

    Changed the functions

--- src/Main/GitMaths.java
+++ src/Main/GitMaths.java
@@ -2,6 +2,17 @@ package Main;
 
 public class GitMaths {
 
-	
+	public void add() {
+		int total = 2 + 3;
+
+		System.out.println("The total is 5");
+
+	}
+
+	public void subtract() {
+		int total = 6 - 4;
+		System.out.println(" the total is :" + total);
+
+	}
 	
 }
